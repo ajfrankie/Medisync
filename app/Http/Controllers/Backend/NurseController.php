d8bfcc39e55508e08274d6655ce2d64fc0ee70a8
Franklin AJ <franklin@konekt.lk>
2025-10-16 15:19:16 +0530

    Update: update Nurse Module

---
+++ app/Http/Controllers/Backend/NurseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\App;
+
+class DoctorController extends Controller
+{
+    //
+}
